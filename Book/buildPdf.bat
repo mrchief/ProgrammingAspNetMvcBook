915ba828bd23108e119f2e6810db2325efba6215
Jess Chadwick <jesschadwick@gmail.com>
2011-11-30 11:33:25 -0500

    Updating PDF build script

---
+++ Book/buildPdf.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+if not exist "%~dpn0.sh" echo Script "%~dpn0.sh" not found & exit 2
+
+set _CYGBIN=C:\cygwin\bin
+if not exist "%_CYGBIN%" echo Couldn't find Cygwin at "%_CYGBIN%" & exit 3
+
+:: Resolve ___.sh to /cygdrive based *nix path and store in %_CYGSCRIPT%
+for /f "delims=" %%A in ('%_CYGBIN%\cygpath.exe "%~dpn0.sh"') do set _CYGSCRIPT=%%A
+for /f "delims=" %%A in ('%_CYGBIN%\cygpath.exe "%CD%"') do set _CYGDIR=%%A
+
+echo %_CYGDIR%
+echo %_CYGSCRIPT%
+
+:: Throw away temporary env vars and invoke script, passing any args that were passed to us
+endlocal & %_CYGBIN%\bash --login -c "cd %_CYGDIR%;%_CYGSCRIPT%" %*
